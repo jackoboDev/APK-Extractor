5df877fdb0d491fdcbadfb1808fd4d59e031f624
Jackobo Tori <97413996+Jack0b0Tori@users.noreply.github.com>
2022-01-18 10:26:42 +0100

    Update ApkListAdapter.java

--- src/axp/tool/apkextractor/ApkListAdapter.java
+++ src/axp/tool/apkextractor/ApkListAdapter.java
@@ -212,9 +212,8 @@ public class ApkListAdapter extends RecyclerView.Adapter<ApkListAdapter.ViewHold
 				String package1 = o1.packageName;
 				String package2 = o2.packageName;
 				
-				PackageManager packageManager= MainActivity.this.getPackageManager();
-				String appName_package1 = (String) packageManager.getApplicationLabel(packageManager.getApplicationInfo(appName_package1, PackageManager.GET_META_DATA));
-				String appName_package2 = (String) packageManager.getApplicationLabel(packageManager.getApplicationInfo(appName_package2, PackageManager.GET_META_DATA));
+				String appName_package1 = (String) mActivity.getApplicationLabel(packageManager.getApplicationInfo(appName_package1, PackageManager.GET_META_DATA));
+				String appName_package2 = (String) mActivity.getApplicationLabel(packageManager.getApplicationInfo(appName_package2, PackageManager.GET_META_DATA));
 
 				return appName_package1.compareTo(appName_package2);
 			}
